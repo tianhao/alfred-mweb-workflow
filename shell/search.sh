b6c0e73dee79c974718ff91b73f7631342fe11e1
tianhao <tianhaolsk@qq.com>
2017-10-12 19:36:01 +0800

    优化按标题匹配度排序

--- shell/search.sh
+++ shell/search.sh
@@ -19,37 +19,48 @@ find_files(){
        exit 1
     fi
     cd "${MDOC_HOME}/docs"
-    local final_expr="ls -lt *.md | awk '{print \$9}'"
+
+    local final_expr="ls -ltr *.md | awk '{print \$9}'"
     for i in $*
     do
         final_expr="${final_expr}| xargs grep -i ${i} | awk -F':' '{print \$1}' | uniq "
     done
-    files=`eval "${final_expr}"`
-    
-    
 
-    #把标题带有关键字的文件的移动到前面
-    filesWithKeywordArray=()
-    
-    count=0
-    filesArray=( $files )
-#    echo $1
-#    echo ${filesArray[@]}
-     
-    for i in ${filesArray[@]} 
-    do
-        filename=$(head -1 $i)
-        filename="$(tr [A-Z] [a-z] <<< "$filename")"    #转换大小写
-        keyword="$(tr [A-Z] [a-z] <<< "$1")"            #转换大小写
-        if [[ ${filename} == *"$keyword"* ]]; then
-            filesWithKeywordArray+="$i "
-            unset filesArray[$count]
-        fi
-        let count=count+1
-    done
-    
-    filesArray=( ${filesWithKeywordArray[@]} ${filesArray[@]} )
-    files=`echo ${filesArray[@]}`
+    # 排序表达式: 统计第一行匹配关键字个数，将匹配个数大的放在前面
+    # 第一步: 输入"文件名",输出"文件名 匹配个数"
+    # 第二步: 按照 "匹配个数" 倒序排序
+    # 第三步: 去掉 "匹配个数" 字段，只保留"文件名"
+    # 由于ls -ltr 是按照编辑时间倒序排序的，所以最终排序等级：标题匹配个数倒序->最后编辑倒序
+    local egrep_expr=$(echo "$*" | sed "s/[[:blank:]]/|/g")
+    local sort_expr="awk '{system(\"egrep -io \\\"${egrep_expr}\\\" <<< \`head -1 \"\$1 \"\`|wc -l | xargs echo \"\$1)}' | sort -rk 2 | awk '{print \$1}'"
+
+    final_expr="${final_expr} | ${sort_expr}"
+    # echo "${final_expr}"
+    files=`eval "${final_expr}"`
+
+##### 由于执行速度不够，已经由前面排序表达式完成了这个功能，而且只使用了第一个关键字匹配，上面表达式按照最大匹配度排序
+# #把标题带有关键字的文件的移动到前面
+# filesWithKeywordArray=()
+#
+# count=0
+# filesArray=( $files )
+##  echo $1
+##  echo ${filesArray[@]}
+#
+#    for i in ${filesArray[@]}
+#    do
+#   filename=$(head -1 $i)
+#   filename="$(tr [A-Z] [a-z] <<< "$filename")"  #转换大小写
+#   keyword="$(tr [A-Z] [a-z] <<< "$1")"      #转换大小写
+#   if [[ ${filename} == *"$keyword"* ]]; then
+#     filesWithKeywordArray+="$i "
+#     unset filesArray[$count]
+#        fi
+#        let count=count+1
+#    done
+#
+# filesArray=( ${filesWithKeywordArray[@]} ${filesArray[@]} )
+# files=`echo ${filesArray[@]}`
 }
 
 output(){
---
+++ shell/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+MDOC_HOME='请改成你的实际目录'
+
+./search.sh $*
