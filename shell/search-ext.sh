d301c154ba349e85c0495187245eb2c7dc4dd416
tianhao <tianhaolsk@qq.com>
2017-11-25 22:53:20 +0800

    修复 https://github.com/tianhao/alfred-mweb-workflow/issues/4 ; 2.提升速度

--- shell/search-ext.sh
+++ shell/search-ext.sh
@@ -45,8 +45,8 @@ do
     final_expr="${final_expr} find \"${i}\" -type f -iname '*.md';"
 done
 unset IFS
-
-final_expr="{${final_expr}} | xargs -I'{}' stat -f'%m %N' '{}' | sort -rn | cut -d' ' -f 2-"  # | xargs -I'{}' grep -l tomcat '{}"
+final_expr="{${final_expr}}"
+#final_expr="{${final_expr}} | xargs -I'{}' stat -f'%m %N' '{}' | sort -rn | cut -d' ' -f 2-"  # | xargs -I'{}' grep -l tomcat '{}"
 #echo "${final_expr}"
 
 
@@ -68,18 +68,26 @@ if [ ${#keyword_arr[@]} -gt 0 ];then
         final_expr="${final_expr}| xargs -I'{}' grep -ile '${i}' '{}' | awk -F':' '{print \$1}' | uniq "
     done
 
+    # 按时间排序
+    # final_expr="${final_expr} | xargs -I'{}' stat -f'%m %N' '{}' | sort -rn | cut -d' ' -f 2- "
+    final_expr="${final_expr} | xargs -I'{}' ls -t '{}'"
+
     # 排序表达式: 统计第一行匹配关键字个数，将匹配个数大的放在前面
     # 第一步: 输入"文件名",输出"文件名 匹配个数"
     # 第二步: 按照 "匹配个数" 倒序排序
     # 第三步: 去掉 "匹配个数" 字段，只保留"文件名"
     # 由于ls -lt 是按照编辑时间倒序排序的，所以最终排序等级：标题匹配个数倒序->最后编辑倒序
     egrep_expr=$(echo "${keyword_arr[@]}" | sed "s/[[:blank:]]/|/g")
-#    sort_expr="awk -F'\n' '{system(\"egrep -ioe \\\"${egrep_expr}\\\" <<< \`head -1 \"substr(\$1,\$100) \"\`|wc -l | xargs echo \"substr(\$1,\$100))}' | sort -rk 2 | awk '{print \$1}'"
     sort_expr="awk -F'\\n' '{system(\"egrep -ioe \\\"${egrep_expr}\\\" <<< \`head -1 \\\"\"\$1 \"\\\"\` | wc -l | xargs -I\\\"{}\\\" echo \\\"{}\\\" \"\$1)}' | sort -r | cut -d' ' -f 2-"
     final_expr="${final_expr} | ${sort_expr} "
+else
+    # 按时间排序
+    # final_expr="${final_expr} | xargs -I'{}' stat -f'%m %N' '{}' | sort -rn | cut -d' ' -f 2- "
+    final_expr="${final_expr} | xargs -I'{}' ls -t '{}'"
 fi
 
-final_expr="${final_expr} | head -20 " # 限制最多输出20条记录
+final_expr="${final_expr} | head -20" # 取前20个
+
 
 echo "{\"items\":["
 n=0
